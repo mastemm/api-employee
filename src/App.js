d778e94b5d5a4c0a8670bb670af9f9783dd300ea
MassimaMao <mastem@msn.com>
2022-04-12 12:27:23 +0200

    employees exercices

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayEmployee from './components/DisplayEmployee';
+import axios from 'axios';
+import { useState } from 'react';
 
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+
+};
 function App() {
+  const [employee, setEmployee] = useState(null);
+
+  const getEmployee = () => {
+    // Send the request  
+    axios  
+      .get('https://randomuser.me/api?nat=en')  
+      // Extract the DATA from the received response  
+      .then((response) => response.data)  
+      // Use this data to update the state  
+      .then((data) => {  
+        setEmployee(data.results[0]);  
+      });
+  
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayEmployee employee={employee}/>
+      <button type="button" onClick={getEmployee}>Get employee</button>
     </div>
   );
 }
